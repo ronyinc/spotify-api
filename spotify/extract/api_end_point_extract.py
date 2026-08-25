4ded4124ba2035f3861347f4c5c56bbf7035ce8c
Rony Boban <ronyinc@hotmail.com>
2026-08-24 17:41:42 -0700

    added code to fetch the data for user saved tracks

--- spotify/extract/api_end_point_extract.py
+++ spotify/extract/api_end_point_extract.py
@@ -38,3 +38,41 @@ def get_recent_tracks(access_token):
         except requests.exceptions.RequestException as e:
              print(f"Failed to fetch recently played tracks: {e}")
              raise
+
+
+def get_users_saved_tracks(access_token):
+
+    headers = {
+          "Authorization": f"Bearer {access_token}"
+     }
+
+    saved_tracks = []
+    data_all = []
+
+    url = f"{BASE_URL}/me/tracks"
+
+    while url:
+        response = requests.get(
+               url,
+               headers=headers,
+               params={"limit": 25},
+          )
+
+        response.raise_for_status()
+
+        data = data = json.loads(response.text) # parse json into py dic.
+        # data_all.append(data)
+        saved_tracks.extend(data["items"])
+
+        print(f"fetched {len(data['items'])} records")
+
+        url = data["next"]
+        params = None
+
+    return saved_tracks      
+
+
+
+
+
+
